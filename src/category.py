da03906bd41fbb6694a29e9cd3b152d2d7e4547e
Your Name <your_email@example.com>
2026-06-24 22:24:30 +0300

    Сделал задание 3 по 14_2

--- src/category.py
+++ src/category.py
@@ -1,14 +1,39 @@
+from src.product import Product
 
 class Category:
     name: str
     description: str
-    products: list
+    __products: list
     category_count = 0
     product_count = 0
 
     def __init__(self, name, description, products):
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products
         Category.category_count += 1
         Category.product_count += len(products)
+
+
+    def add_product(self, product):
+        self.__products.append(product)
+        Category.product_count += 1
+
+
+    @property
+    def products(self):
+        list_products = []
+
+        for product in self.__products:
+            list_products.append(f'{product.name}, {product.price} руб. {product.quantity}: 15 шт.')
+
+        return list_products
+
+
+
+
+
+
+
+
+
