7560ce4f3653440a090ca6624c2fbb2b7919b163
Your Name <your_email@example.com>
2026-06-30 21:58:40 +0300

    Сделал дополнительное задание по итерации и тест к нему.

---
+++ tests/test_product_iterator.py
@@ -0,0 +1,10 @@
+from src.product_iterator import ProductIterator
+
+def test_product_iterator(categori_class_init_2):
+    iterator = ProductIterator(categori_class_init_2)
+    assert iterator.index == 0
+    assert next(iterator).name == 'Перчатки'
+    assert iterator.index == 1
+    assert next(iterator).name == 'Цемент'
+    assert iterator.index == 2
+    assert next(iterator).name == 'Тачанка'
