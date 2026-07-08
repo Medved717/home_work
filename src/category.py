01a5a1b7e01031bd056cdacf46e6cfe09483b2f4
Дмитрий Наумов <arsmdll@mail.ru>
2026-07-09 00:15:09 +0300

    Сделал задания с первого по третье включительно.

--- src/category.py
+++ src/category.py
@@ -19,8 +19,11 @@ class Category:
         Category.product_count += len(products)
 
     def add_product(self, product):
-        self.__products.append(product)
-        Category.product_count += 1
+        if isinstance(product, Product):
+            self.__products.append(product)
+            Category.product_count += 1
+        else:
+            raise TypeError('Неподходящий тип продукта.')
 
     # Не уверен, что данный код нужен, просто хотел оставить геттер, который просто возвращает атрибут????
     @property
